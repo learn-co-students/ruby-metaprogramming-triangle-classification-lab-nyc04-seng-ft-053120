9eb6657599c5e9840ce99cb25f29b3eb5c21cc26
peawarrior <jacksonchen171@gmail.com>
2020-06-29 21:57:24 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @all_sides = [a, b, c]
+  end
+
+  def valid_triangle?
+    valid = [(a + b > c), (b + c > a), (c + a > b)]
+    @all_sides.each do |side|
+      valid << false if side <= 0
+    end
+    valid.include?(false) ? false : true
+  end
+
+  def kind
+    if !valid_triangle?
+        raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
